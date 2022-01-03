be2d64be9bc1e301de19f064232b9ac2675553f1
HyunJay52 <hyunjungk52@gmail.com>
2022-01-04 00:26:09 +0900

    final movie app

--- src/App.js
+++ src/App.js
@@ -1,100 +1,65 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-/*
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 3.5
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 5
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 2
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4
-  }
-];
-
-function Food({name, picture, rating}){
-  return (
-    <div>
-      <h3>I love {name}</h3>
-      <h5>{rating}/5.0m</h5>
-      <img src={picture} alt={name} />
-    </div>
-  )
-}
-
-function renderFood(dish){
-  return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-}
-
-// propType는 component의 데이터 타입을 체크해주는 역할
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
- // isRequired 없으면 unfined도 해당  
-}
-
-function App() {
-  return (
-    <div> {foodILike.map(renderFood)}</div>
-  );
-}
-*/
-
-class App extends React.Component{
+class App extends React.Component {
 
   state = {
-    //state is keeping data that can be changed
-    count: 0
+    isLoading: true,
+    movies: []
   };
 
-  //js code
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-    // whenever react calls setState that will call render function with new state!
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
+  getMovies = async () => {
+    // axios가 준비 될 때까지 기다리는 것 async function - await 
+    const {
+      data: {
+        data: {
+          movies
+        }
+      }
+    } = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+
+    this.setState({
+      movies,
+      isLoading: false
+    });
   };
 
-  render(){
+  componentDidMount() {
+    this.getMovies();
+  };
+
+  render() {
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        <h1>this is state value : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-                          {/* add() means right away~ */}
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    )
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+
   }
+
+
 }
 
 export default App;
